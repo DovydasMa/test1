fb4377ba0d023aacab800007d665e61dc0512676
DovydasMa <105418683+DovydasMa@users.noreply.github.com>
2022-12-11 22:48:51 +0200

    Update z.php

--- z.php
+++ z.php
@@ -1,15 +1,6 @@
 <?php
-
-
-use Twilio\Rest\Client;
-// Update the path below to your autoload.php,
-// see https://getcomposer.org/doc/01-basic-usage.md
 require_once '/var/www/html/vendor/autoload.php';
-
-
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-
+use Twilio\Rest\Client;
 $x = $_POST["x"];
 
 
@@ -20,42 +11,35 @@ $x = $_POST["x"];
 
 
 
-// Create connection
-$conn = mysqli_connect($IP, "testuser", "testpass", "data");
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
 
 
 
-$sql = "INSERT INTO data.Number (phoneNum)
-VALUES ($x)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-
-
-$sid = "AC88455ae15ef68ccb9c6269800700191c";
-$token = "5e680d305be0aecfa47415df124e1e79";
-$twilio = new Client($sid, $token);
-
-$call = $twilio->calls
-               ->create($x, // to
-                        "+16506401992", // from
-                        [
-                            "twiml" => "<Response><Say>Ahoy, World!</Say></Respon>
-                        ]
-               );
-
-print($call->sid);
 
 
 
+
+
+
+
+// Your Account SID and Auth Token from twilio.com/console
+$account_sid = 'AC88455ae15ef68ccb9c6269800700191c';
+$auth_token = '5e680d305be0aecfa47415df124e1e79';
+// In production, these should be environment variables. E.g.:
+// $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
+
+// A Twilio number you own with Voice capabilities
+$twilio_number = "+16506401992";
+
+// Where to make a voice call (your cell phone?)
+$to_number = $x;
+
+$client = new Client($account_sid, $auth_token);
+$client->account->calls->create(
+    $to_number,
+    $twilio_number,
+    array(
+        "url" => "http://demo.twilio.com/docs/voice.xml"
+    )
+);
 
 
